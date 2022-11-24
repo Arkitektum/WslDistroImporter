0beb2add5ac3f818cb5f41c44f4f7a28c4ed7b0b
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2022-11-24 09:29:15 +0100

    Oppdaterte hjelpetekster

--- WslDistroImporter/Options.cs
+++ WslDistroImporter/Options.cs
@@ -4,13 +4,13 @@ namespace WslDistroImporter
 {
     public class Options
     {
-        [Option('n', "distro-name", Required = true, HelpText = "Distro name")]
+        [Option('n', "distro-name", Required = true, HelpText = "Your WSL distro name.")]
         public string DistroName { get; set; }
-        [Option('i', "image", SetName = "image", Required = true, HelpText = "Docker image")]
+        [Option('i', "image", SetName = "image", Required = true, HelpText = "Docker image. Must be present locally or at hub.docker.com.")]
         public string Image { get; set; }
-        [Option('f', "dockerfile", SetName = "dockerfile", Required = true, HelpText = "Path of Dockerfile")]
+        [Option('f', "dockerfile", SetName = "dockerfile", Required = true, HelpText = "Path of Dockerfile.")]
         public string Dockerfile { get; set; }
-        [Option('l', "install-location", Required = true, HelpText = "Install location")]
+        [Option('l', "install-location", Required = true, HelpText = "Where to install WSL distro.")]
         public string InstallLocation { get; set; }
     }
 }
